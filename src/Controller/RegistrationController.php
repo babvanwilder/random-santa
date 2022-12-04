257dfcfb9b1d86c3ffa1dc35f0dd97aad8d725f4
Van WILDER <ugobab@hotmail.fr>
2022-12-04 17:32:23 +0100

    block non codes rousseau email and fix email verify and activate account

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,28 +4,38 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
-use App\Security\EmailVerifier;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Manager\UserManager;
+use Psr\Log\LoggerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
+use Throwable;
 
 class RegistrationController extends AbstractController
 {
-    private EmailVerifier $emailVerifier;
+    public function __construct(
+        private readonly VerifyEmailHelperInterface $verifyEmailHelper
+    ) {}
 
-    public function __construct(EmailVerifier $emailVerifier)
-    {
-        $this->emailVerifier = $emailVerifier;
-    }
-
+    /**
+     * @throws TransportExceptionInterface
+     */
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        UserManager $userManager,
+        MailerInterface $mailer,
+        LoggerInterface $logger
+    ): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -39,55 +49,76 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
-
-            $user->setRoles(["ROLE_USER"]);
-
-            $entityManager->persist($user);
-            $entityManager->flush();
-
+            $userManager->save($user);
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('ramdomsanta@codes-rousseau.fr', 'Random Santa'))
-                    ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
+            $signature = $this->verifyEmailHelper->generateSignature(
+                'app_verify_email',
+                $user->getId(),
+                $user->getEmail(),
+                [
+                    'id' => $user->getId()
+                ]
             );
-            // do anything else you need here, like send an email
+
+            $logger->info("id: {$user->getId()} | email: {$user->getEmail()}");
+
+            $email = (new TemplatedEmail())
+                ->from(new Address('ramdomsanta@codes-rousseau.fr', 'Random Santa'))
+                ->to(new Address($user->getEmail(), $user->getFirstname() . " " .$user->getLastname()))
+                ->subject('Confirmation de votre email')
+                ->htmlTemplate('registration/confirmation_email.html.twig')
+                ->context([
+                    'signedUrl' => $signature
+                ]);
+            ;
+
+            $mailer->send($email);
 
             return $this->redirectToRoute('app_succeeded_register');
         }
 
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form->createView(),
+        return $this->renderForm('registration/register.html.twig', [
+            'form' => $form,
         ]);
     }
 
     #[Route('/register/succeeded', name: 'app_succeeded_register')]
     public function succeeded(): Response
     {
-        return $this->render('register/succeeded.html.twig', [
-            'controller_name' => 'RegistrationController',
-        ]);
+        return $this->render('registration/succeeded.html.twig');
     }
 
     #[Route('/verify/email', name: 'app_verify_email')]
-    public function verifyUserEmail(Request $request): Response
+    public function verifyUserEmail(Request $request, UserManager $userManager, LoggerInterface $logger): Response
     {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $id = $request->get('id');
 
+        if (null === $id) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $user = $userManager->getOne($id);
+
+        if (null === $user) {
+            return $this->redirectToRoute('app_home');
+        }
         // validate email confirmation link, sets User::isVerified=true and persists
         try {
-            $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
+            $logger->info(get_class($this->verifyEmailHelper));
+            $this->verifyEmailHelper->validateEmailConfirmation(
+                $request->getUri(),
+                $user->getId(),
+                $user->getEmail()
+            );
+            $userManager->save($user->setIsVerified(true));
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $exception->getReason());
 
             return $this->redirectToRoute('app_register');
         }
 
-        // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('success', 'Votre compte a bien été vérifié.');
 
-        return $this->redirectToRoute('app_home');
+        return $this->redirectToRoute('app_login');
     }
 }
